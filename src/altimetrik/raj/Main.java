1d0e9a9ebe9a228155b4ac0c46e520b116fc48e1
Raj_Kumar <rajkumar.innocube@gmail.com>
2020-11-06 17:06:34 +0530

    Initial commit

---
+++ src/altimetrik/raj/Main.java
@@ -0,0 +1,19 @@
+package altimetrik.raj;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	* String
+	* String is not a primitive datatype
+	* it is a class in java
+	*/
+
+        String nameEmployee="Rajkumar";
+        String eidEmployee="36";
+        String mergeValue=nameEmployee+eidEmployee;
+        System.out.println(mergeValue);
+        mergeValue=mergeValue+"Software Develpoer Trainee";
+        System.out.println(mergeValue);
+    }
+}
